a03d6732b5950b517d66e351610a278062444a89
BlueManDev <96463917+BlueManDev@users.noreply.github.com>
2022-04-12 13:33:16 +1000

    Create LevelEditorScene.java

---
+++ src/main/java/engine/LevelEditorScene.java
@@ -0,0 +1,15 @@
+package engine;
+
+public class LevelEditorScene extends Scene
+{
+    public LevelEditorScene()
+    {
+
+    }
+
+    @Override
+    public void update(float dt)
+    {
+
+    }
+}
