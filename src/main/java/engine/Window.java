760c4cd8ca307923af50be0af409a8362687e24a
BlueManDev <96463917+BlueManDev@users.noreply.github.com>
2022-04-12 13:26:02 +1000

    Update Window.java

--- src/main/java/engine/Window.java
+++ src/main/java/engine/Window.java
@@ -4,6 +4,8 @@ import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
 
+import util.Time;
+
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.NULL;
